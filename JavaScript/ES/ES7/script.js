77517b33adb95f64fad7873d939e3f4f8028ec64
Mariano-JR <mariano_br123@hotmail.com>
2022-09-15 20:14:42 -0300

    Adição de novos conteudos do curso

---
+++ JavaScript/ES/ES7/script.js
@@ -0,0 +1,16 @@
+const pets = [
+    'cat',
+    'dog',
+    'bat'
+];
+
+pets.includes('dog'); // True
+pets.includes('bird'); // False
+
+/--------------------------------/
+
+const square = (x) => x**2;
+const cube = (y) => y**3;
+
+square(5); // 25
+cube(4); // 64
